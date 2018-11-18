82371ff1bd824b3b4864eeff0afd4d85475d7d5b
doanh <doanh@pdx.edu>
2018-11-17 20:33:48 -0800

    implemented score method

--- hexapawn.py
+++ hexapawn.py
@@ -1,4 +1,9 @@
-import copy
+import utility_methods as util
+import sys
+
+# player notation:
+#  -1 -> White
+#   1 -> Black
 
 initial_board = [
     ['W', 'W', 'W'],
@@ -7,113 +12,60 @@ initial_board = [
 ]
 
 
-def print_twod_array(twoDarray):
-    for row in twoDarray:
-        for elem in row:
-            print elem,
-        print("\n")
-
-
-def get_positions(board, player):
-    list_of_coordinates = []
-    for index_row in range(len(board)):
-        for index_col in range(len(board)):
-            if board[index_row][index_col] == player:
-                list_of_coordinates.append((index_row, index_col))
-    return list_of_coordinates
-
-
-def generate_move(board, player):
-    list_of_moves = []
-    list_of_coordinates = get_positions(board, player)
-    for coordinate in list_of_coordinates:
-        temp_list = move(board, coordinate[0], coordinate[1])
-        for a_board in temp_list:
-            list_of_moves.append(a_board)
-
-    return list_of_moves
-
-
-def valid_position(length, row, col):
-    if row >= 0 and row < length and col >= 0 and col < length:
-        return True
-    return False
-
-
-def move(board, row, col):
-    list_of_move = []
-    length = len(board)
-    if board[row][col] == 'B':
-        move_up_row = row - 1
-        if valid_position(length, move_up_row, col) and board[move_up_row][col] == '0':
-            new_board = copy.deepcopy(board)
-            new_board[row][col] = '0'
-            new_board[move_up_row][col] = 'B'
-            list_of_move.insert(0, new_board)
-        diagonal_left_row = row - 1
-        diagonal_left_col = col - 1
-        if valid_position(length, diagonal_left_row, diagonal_left_col) \
-                and board[diagonal_left_row][diagonal_left_col] == 'W':
-            new_board = copy.deepcopy(board)
-            new_board[row][col] = '0'
-            new_board[diagonal_left_row][diagonal_left_col] = 'B'
-            list_of_move.insert(0, new_board)
-
-        diagonal_right_row = row - 1
-        diagonal_right_col = col + 1
-        if valid_position(length, diagonal_right_row, diagonal_right_col) \
-                and board[diagonal_right_row][diagonal_right_col] == 'W':
-            new_board = copy.deepcopy(board)
-            new_board[row][col] = '0'
-            new_board[diagonal_right_row][diagonal_right_col] = 'B'
-            list_of_move.insert(0, new_board)
-        return list_of_move
-    else:
-        move_down_row = row + 1
-        if valid_position(length, move_down_row, col) and board[move_down_row][col] == '0':
-            new_board = copy.deepcopy(board)
-            new_board[row][col] = '0'
-            new_board[move_down_row][col] = 'W'
-            list_of_move.insert(0, new_board)
-        diagonal_left_row = row + 1
-        diagonal_left_col = col - 1
-        if valid_position(length, diagonal_left_row, diagonal_left_col) \
-                and board[diagonal_left_row][diagonal_left_col] == 'B':
-            new_board = copy.deepcopy(board)
-            new_board[row][col] = '0'
-            new_board[diagonal_left_row][diagonal_left_col] = 'W'
-            list_of_move.insert(0, new_board)
-
-        diagonal_right_row = row + 1
-        diagonal_right_col = col + 1
-        if valid_position(length, diagonal_right_row, diagonal_right_col) \
-                and board[diagonal_right_row][diagonal_right_col] == 'B':
-            new_board = copy.deepcopy(board)
-            new_board[row][col] = '0'
-            new_board[diagonal_right_row][diagonal_right_col] = 'W'
-            list_of_move.insert(0, new_board)
-        return list_of_move
-
-
 class node:
-    def __init__(self, i_depth,  board, i_player, value = 0):
+    def __init__(self, i_depth,  board, player, value=0):
         self.depth = i_depth
         self.board = board
-        self.player = i_player
+        self.player = player
         self.value = value
         self.children = []
+        self.create_children()
+
+    def get_value(self):
+        return self.value
+
+    def get_player(self):
+        if self.player == 1:
+            return 'B'
+        return 'W'
+
+    def create_children(self):
+        # check for depth here when iterative deepening
+        possible_move = util.generate_move(self.board, self.get_player())
+        if not possible_move:
+            return
+        # for a_board in possible_move:
+        #     temp_node =
+
+    def check_score(self):
+        # if Black player
+        if self.player == 1:
+            for pawn in self.board[0]:
+                if pawn == self.get_player():
+                    self.value = self.player * sys.maxint
+                    return
+            score = (len(util.get_positions(self.board, 'B')) - (len(util.get_positions(self.board, 'W'))))
+            # leaf node and game is tied
+            if score == 0 and not self.children:
+                self.value = self.player * sys.maxint
+            else:
+                self.value = score
+
+        # White Player's turn
+        else:
+            for pawn in self.board[len(self.board) - 1]:
+                if pawn == self.get_player():
+                    self.value = self.player * sys.maxint
+                    return
+            score = (len(util.get_positions(self.board, 'W')) - (len(util.get_positions(self.board, 'B'))))
+            score = score * self.player
+            # leaf node and game is tied
+            if score == 0 and not self.children:
+                self.value = self.player * sys.maxint
+            else:
+                self.value = score
 
 
-
-
-# result = move(initial_board, 0, 2)
-# for elem in result:
-#     print_twod_array(elem)
-#     print("*****************")
-
-##print(get_positions(initial_board, 'W'))
-
-templist = generate_move(initial_board, 'B')
-for elem in templist:
-    print_twod_array(elem)
-    print("*****************")
+root = node(0, initial_board, -1)
+root.check_score()
+print(root.get_value())
